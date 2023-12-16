df2233b8beecf8729f2e436916bc056b7ba39f67
Kristoff Lutchman <info@kristofflutchman.com>
2023-12-15 18:43:58 -0800

    added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+It's December 2023 and Google has made their Gemini API available for free to us tinkerers. Here you'll find information to quickly get up and running with a gradio chatbot using the Google's API. At the link below, Google provides a link to get yourself an API key. It's a good idea to add the key as an environment variable as done here in this code.
+
+Google's quickstart guide for python: https://ai.google.dev/tutorials/python_quickstart
+
+Accompanying YouTube video:
--- gemini-chat-app.py
+++ gemini-chat-app.py
@@ -6,16 +6,7 @@ def gemini_chat(message, history):
     response = chat.send_message(message)
     return response.text
 
-css = """
-#text {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
-"""
-
 API_KEY = os.getenv('GOOGLE_API_KEY')
-
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel('gemini-pro')
@@ -24,7 +15,14 @@ chat = model.start_chat()
 iface = gr.ChatInterface(
     fn=gemini_chat, 
     title='Gemini Chat',
-    chatbot=gr.Chatbot(height='100%')
+    chatbot=gr.Chatbot(height=1000),
+    textbox=gr.Textbox(
+        placeholder="Message Gemini",
+        scale=7
+    ),
+    retry_btn=None,
+    undo_btn=None,
+    clear_btn=None
 )
 
 if __name__ == '__main__':
